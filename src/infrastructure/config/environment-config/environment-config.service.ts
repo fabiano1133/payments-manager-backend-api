84b5bc6be859b01eb62fa58650c549dfd630a586
fabiano1133 <fabiano.santos0@icloud.com>
2023-05-20 10:34:18 -0300

    changes

--- src/infrastructure/config/environment-config/environment-config.service.ts
+++ src/infrastructure/config/environment-config/environment-config.service.ts
@@ -10,7 +10,7 @@ export class EnvironmentConfigService implements IDatabase {
     return this.configService.get<string>('DATABASE_HOST_PROD');
   }
   getDatabasePort(): number {
-    return this.configService.get<number>('PORT');
+    return this.configService.get<number>('DATABASE_PORT');
   }
   getDatabaseUser(): string {
     return this.configService.get<string>('DATABASE_USER');
